b756236c2f96fa6edf59bfa6d5200a3397a8a1b7
Wilhelm Murdoch <wilhelm@devilmayco.de>
2024-01-10 23:23:55 +1000

    cleaning utility for trivias

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,10 @@
 <div class="prose">
     <ul>
         {#each data.quiz.questions as q}
-            <h3>{q.question}</h3>
+            <h3><span class="mr-1 text-slate-500">{q.qid}.</span>{q.text}</h3>
             <ol>
                 {#each q.choices as a}
-                    <li class:text-emerald-600={a.correct}>{a.text}</li>
+                    <li class:text-emerald-600={a.is_correct}>{a.text}</li>
                 {/each}
             </ol>
         {/each}
