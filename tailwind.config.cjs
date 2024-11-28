5d8810182d92fd57f7f0f3a03eccc80eb10aa412
Wilhelm Murdoch <wilhelm@devilmayco.de>
2024-11-28 19:34:49 +1000

    nfi what I was doing with this

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}'
-	],
-	plugins: [
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/typography')
-	],
-};
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+
+const config = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography')
+	]
+} satisfies Config;
+
+export default config;
+						
