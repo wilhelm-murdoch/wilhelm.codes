2e47ed657f643bb8d5086790a645be45a63802f4
Wilhelm Murdoch <wilhelm@devilmayco.de>
2025-01-25 01:18:26 +1000

    more progress on the changelog

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -4,7 +4,7 @@ const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../content/*.md", 
+    "../content/*.md",
     "../layouts/**/*.html"
   ],
   safelist: [
@@ -17,8 +17,9 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/typography'),
-    plugin(function ({addVariant}) {
+    plugin(function ({ addVariant }) {
       addVariant('prose-inline-code', '&.prose-xl :where(:not(pre)>code):not(:where([class~="not-prose"] *))');
+      addVariant('not-last', '&:not(:last-child)')
     })
   ],
 };
