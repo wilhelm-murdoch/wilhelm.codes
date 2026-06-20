3a2afa7b0267a4106cfea089db90760212eaf7b2
Wilhelm Murdoch <wilhelm@devilmayco.de>
2026-06-20 18:11:33 +1000

    add a new icon for snippet bits

--- scripts/fetch-icons.sh
+++ scripts/fetch-icons.sh
@@ -73,6 +73,7 @@ fetch play                       ph:play-duotone                        "${UI_DI
 fetch arrow-up-right             ph:arrow-up-right-duotone              "${UI_DIR}"
 fetch chevron-down               ph:caret-down-duotone                  "${UI_DIR}"
 fetch terminal-window            ph:terminal-window-duotone             "${UI_DIR}"
+fetch hash-straight              ph:hash-straight-duotone               "${UI_DIR}"
 
 # Git / changelog activity (Phosphor duotone)
 fetch code-commit                ph:git-commit-duotone                  "${UI_DIR}"
